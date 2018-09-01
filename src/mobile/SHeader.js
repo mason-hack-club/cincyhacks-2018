9e3391cdd421fc14f8f063b37d6be40ba0add570
Megan Cui <megan.cui.mc@gmail.com>
2018-09-01 18:33:36 -0400

    added application link

--- src/mobile/SHeader.js
+++ src/mobile/SHeader.js
@@ -3,6 +3,10 @@ import './Mobile.css';
 import logo from '../chlogo.png';
 
 class SHeader extends React.Component {
+  handleClick = () => {
+    window.location = "https://docs.google.com/forms/d/e/1FAIpQLSd7h-synW9Serf1norecyFGU3XcIvW5GrwY-2s7fkjrON-86Q/viewform?usp=sf_link";
+  }
+
   render() {
     return(
       <div className="header__container">
@@ -11,7 +15,7 @@ class SHeader extends React.Component {
           <h2 id="h__desc">Cincinnati's original high school hackathon</h2>
 
         <h2 id="h__date">September {22}nd to {23}rd, {2018}</h2>
-        <button className="apply">Apply now!</button>
+        <button className="apply" onClick={this.handleClick}>Apply now!</button>
       </div>
     )
   }
--- src/mobile/SReady.js
+++ src/mobile/SReady.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './Mobile.css';
 
 class SReady extends React.Component {
+  handleClick = () => {
+    window.location = "https://docs.google.com/forms/d/e/1FAIpQLSd7h-synW9Serf1norecyFGU3XcIvW5GrwY-2s7fkjrON-86Q/viewform?usp=sf_link";
+  }
+
   render() {
     return(
       <div className="ready__container">
         <h1>Ready to join us?</h1>
-        <button className="r__apply">Apply Now!</button>
+        <button className="r__apply" onClick={this.handleClick}>Apply Now!</button>
       </div>
     )
   }
