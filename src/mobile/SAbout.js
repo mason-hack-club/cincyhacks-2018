c7b73dd98e31e9d7b4a1b0bd3a0de1a4d7253015
Megan Cui <megan.cui.mc@gmail.com>
2018-09-02 11:43:56 -0400

    Update SAbout.js

--- src/mobile/SAbout.js
+++ src/mobile/SAbout.js
@@ -6,8 +6,17 @@ class SAbout extends React.Component {
     return(
       <div className="about__container">
         <h1>What is CincyHacks?</h1>
-        <p id="a__desc">CincyHacks is Cincinnati's first hackathon dedicated to high school students. Our mission is to encourage students like you to learn and further explore the growing world of technology. After last year's huge success at CincyHacks 2017, we decided to do it again this year in hopes of reaching more students.</p>
-      </div>
+<p>
+						CincyHacks is Cincinnati's first hackathon dedicated to high school students. Our mission is to encourage students like you to learn and further explore the growing world of technology.
+					</p>
+
+					<p>
+						The brainchild of Mason Hack Club, CincyHacks began in 2017 as a milestone event to increase student involvement in techology. Hosted at the University of Cincinnati, CincyHacks 2017 boasted over 90 attendees from 24 schools and 3 states, made possible by support from our event partner, Cengage Learning, and our other sponsors.
+					</p>
+
+					<p>
+						This year's event is run by <a href="https://masonhackclub.com">Mason Hack Club</a> and in partnership with Mason High School.
+					</p>      </div>
     )
   }
 }
