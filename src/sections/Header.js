4c41094f2b3b1be41c333b4608ce633c08def88f
Christian Galang <41277107+nxvgalang@users.noreply.github.com>
2018-09-05 17:38:05 -0400

    Okay I think I got it

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -17,11 +17,7 @@ class Header extends React.Component {
 
 				<div id="title">
 					<img src={logo} />
-					<h1>CincyHacks</h1>
-					<div id="ch-icons">
-						<a href="https://twitter.com/cincyhacks" class="icon"><i class="fa fa-twitter"></i></a>
-						<a href="https://www.instagram.com/cincyhacks/" class="icon"><i class="fa fa-instagram"></i></a>
-					</div>
+					<h1>CincyHacks   <span id="ch-icons"><a href="https://twitter.com/cincyhacks" class="icon"><i class="fa fa-twitter"></i></a><a href="https://www.instagram.com/cincyhacks/" class="icon"><i class="fa fa-instagram"></i></a></span></h1>
 						<h2 id="h__desc" style={{fontSize: '28px'}}>Cincinnati's original high school hackathon</h2>
 
 					<h2 id="h__date" style={{fontSize: '24px'}}>September 22nd to 23rd, 2018</h2>
