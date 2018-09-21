27f140558235fd455cb80eb738578d419f5b2dfc
Christian Galang <41277107+nxvgalang@users.noreply.github.com>
2018-09-21 19:59:07 -0400

    Change to Resources Section

--- src/mobile/SReady.js
+++ src/mobile/SReady.js
@@ -3,14 +3,14 @@ import './Mobile.css';
 
 class SReady extends React.Component {
   handleClick = () => {
-    window.open("https://docs.google.com/forms/d/e/1FAIpQLSd7h-synW9Serf1norecyFGU3XcIvW5GrwY-2s7fkjrON-86Q/viewform?usp=sf_link");
+    window.open("https://cincyhacks2018.devpost.com/");
   }
 
   render() {
     return(
       <div className="ready__container">
-        <h1>Ready to join us?</h1>
-        <button className="r__apply" onClick={this.handleClick}>Apply Now!</button>
+        <h1>Resources</h1>
+        <button className="r__apply" onClick={this.handleClick}>Submit Project!</button>
       </div>
     )
   }
