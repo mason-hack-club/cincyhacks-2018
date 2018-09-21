8edf043058ab460a8b838e941e4e21829f513671
Christian Galang <41277107+nxvgalang@users.noreply.github.com>
2018-09-21 19:56:08 -0400

    Change to Resources section

--- src/sections/Register.js
+++ src/sections/Register.js
@@ -4,10 +4,7 @@ class Register extends React.Component {
 	render() {
 		return (
 			<div className="section" id="register-container">
-				<h1>Ready to Join?</h1>
-				<form action="https://goo.gl/forms/y8JIe0QPVXsw48uD2" method="get" target="_blank">
-					<button type="submit" className="register-btn" >Apply Now!</button>
-				</form>
+				<h1>Resources</h1>
 			</div>
 		);
 	}
