113bb78712ccfaa32cdb4f27eea38dc52ffc6108
Christian Galang <41277107+nxvgalang@users.noreply.github.com>
2018-09-21 20:01:05 -0400

    Add submit project button

--- src/sections/Register.js
+++ src/sections/Register.js
@@ -5,6 +5,9 @@ class Register extends React.Component {
 		return (
 			<div className="section" id="register-container">
 				<h1>Resources</h1>
+				<form action="https://cincyhacks2018.devpost.com/" method="get" target="_blank">
+					<button type="submit" className="register-btn" >Submit Project</button>
+				</form>
 			</div>
 		);
 	}
